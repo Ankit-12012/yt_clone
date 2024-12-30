ce696a1d18a6bec908fff0656871a80af9e501ff
Ankit <ankitmalik12012@gmail.com>
2024-12-30 19:32:56 +0530

    Run th eback end in the port or initialise app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import "dotenv/config";
 import DB_CONNECTION from "./db/start.js";
+import app from "./app.js";
 
 console.log("env imported :", process.env);
-DB_CONNECTION();
+const port = process.env.PORT || 4000;
+DB_CONNECTION()
+  .then(() => {
+    app.get("/", (req, res) => {
+      res.send("hello");
+    });
+    app.listen(port, () => {
+      console.log("back in running on ", port);
+    });
+  })
+  .catch((err) => {
+    console.log("error in mongodb connection", err);
+  });
